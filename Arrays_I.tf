8ea6b9723a77e6e5a325206b97a19e65c72efeb0
SuperCodeAurora <charles.fang.huang@gmail.com>
2025-12-23 21:19:10 +0800

    Create Arrays_I.tf
    Lesson I about arrays, a fascinating and important concept in TF.

---
+++ Arrays_I.tf
@@ -0,0 +1,5 @@
+box Main() <<<
+@ some global \rack[num]:a }} [1,2,3,E,PI]; //Yes, E and PI count as numbers(even floats do!)
+@ some global \rack[num]\:b }} a.transmit(n }} 2n+1);  //mapping function
+@ io.opt(b);
+>>>

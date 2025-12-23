4fcc65769c4983ffb25dc6cad28652423b421b1f
SuperCodeAurora <charles.fang.huang@gmail.com>
2025-12-23 12:10:03 +0800

    Create Variables.tf
    Learn to define Variables in ThorFlex, an very important and basic concept in the language.

---
+++ Variables.tf
@@ -0,0 +1,6 @@
+box Main() <<< //Welcome back to the Main box!
+@ some global \str\:a }} "I love TF!"; //some is the definition keyword, global defines the scope, between \ \ it is the type. }} is the same as == in most languages and === in js.
+@ some global \str OR num OR bool\:b }} true; //You can also use union types with the OR keyword.
+@ io.opt($/a/); //use $/.../ when you print a variable.
+@ io.opt($/b/); 
+>>> 
